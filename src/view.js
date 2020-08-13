29b50c661f79627b3a1a0c206f400d5062f89e8a
Daniel Rocha <rochadaniel15@gmail.com>
2020-08-13 14:01:49 -0300

    refactor: Habilitado o Vue.js para o desenvolvimento da extensão e implementação inicial do pop-up

--- src/view.js
+++ src/view.js
@@ -1,55 +1,87 @@
-let $ = document.querySelector.bind(document)
-
-function template(list){
-  return `
+new Vue({
+  el: '#app',
+  template: `
     <section>
-      <h1>Manga Progress</h1>
-        <ul>
-          ${list}
-        </ul>
-    </section>
-  `
-}
-
-document.body.onload = function() {
-
-  let app = $('#app')
-  let list = ''
-      
-
-  chrome.storage.sync.get(['favorites'], function( {favorites : mangas } ) {
-
-    for(let key in mangas){
-
-      let manga = new Map(mangas[key])
-
-      list += `
-        <li>
-          <a  href="#" onClick="console.log('ok')">
-             <h3>${ manga.get('title') }</h3>
+      <header id="header-manga-progress">
+        <div v-if="mode != 'read'" class='header-title'>
+          <h1>{{title}}</h1>
+        </div>
+        <div v-else class="card-read">
+          <section>
+             <header>
+                 <img src="https://img-host.filestatic1.xyz/mangas_files/one-piece-br/image_one-piece-br.jpg"/>
+                 <h1>{{mangas[0]?.title}}</h1>     
+             </header>
              <div>
-               <span>Chapters: ${ manga.get('chapters') }</span>
-               <span>Current: ${ manga.get('current') }</span>
+               <span>Chapters: mangas[0]?.chapters</span>
+               <span>Current: mangas[0]?.current</span>
              </div>
-          </a>
-        </li>
-      `
+          </section>
+        </div>
+       <button v-on:click="changeMode">{{mode}}</button>
+      </header>
+      <main class='card-container'>
+          <ul >
+             <li class='card' v-for="manga in mangas">
+               <a 
+                 :href="manga?.href" 
+                 @click.prevent="redirectPage($event)" 
+               > <header>
+                   <img src="https://img-host.filestatic1.xyz/mangas_files/one-piece-br/image_one-piece-br.jpg"/>
+                   <h3>{{manga.title}}</h3>
+                 </header>
+                 <section>
+                   <span>Chapters: {{manga?.chapters}}</span>
+                   <span>Current: {{manga?.current}} </span>
+                 </section>
+                 <input 
+                   type='button' 
+                   :value='(manga?.current)? "Continue" : "Read"'
+                 /> 
+               </a>
+             </li>
+          </ul>
+      </main>
+    </section>
+  `,
+  
+  data: {
+    title: 'Manga Progress',
+    mode: 'read',
+    mangas: [],
+  },
+  methods: {
+  	changeMode(){
+  		this.mode = (this.mode === 'read') ? 'blur' : 'read'
+  	},
+
+  	redirectPage(href){
+       //window.open(href)
+  	}
+  },
+  mounted(){
+
+    // chrome.storage.sync.get(['favorites'], ( {favorites : mangas } ) => {
+      // for(let key in mangas){
+// 
+      	// let manga = new Map(mangas[key])
+// 
+        // this.mangas.push({
+        	// title: manga.get('title'),
+        	// chapters: manga.get('chapters'),
+        	// current: manga.get('current'),
+        	// href: manga.get('address'),
+        // })
+      // }
+    // })
+
+    for(let i = 0; i < 8; i++){
+    	this.mangas.push({
+    		title: 'One Piece',
+    		chapters: 987,
+    		current: 0,
+    		href: '#'
+    	})
     }
-
-    app.innerHTML = template(list)
-     
-  })
-
-}
-
-
-function redirect(href){
-	location.href = href
-}
-
-// new Vue({
-  // el: '#app',
-  // datas: {
-  	// title: "Manga Progress",
-  // },
-// })
+  }
+})
