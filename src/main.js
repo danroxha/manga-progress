208cfe78eb65a50b6cf74722db095c1fe8651ae7
Daniel Rocha <rochadaniel15@gmail.com>
2020-08-17 23:53:08 -0300

    refactor/feat: Mudança no acesso de dados e adicionado nova proprieda    - Corrigido logica de filtro do ID    - Implementação inicial do evento de messagens entre o pop-up e o script de background

--- src/main.js
+++ src/main.js
@@ -26,15 +26,15 @@ function getMangaData(){
 
   try {
 
-    manga['title'] = String($$('.title')[0]?.textContent ?? $('.html-embed-3')?.children[0]?.children[0]?.textContent?.match(/(\w+.*[-])/)[0]?.slice(0, -2)) 
-	  manga['cover'] = String(document.querySelector('.image-3')?.src ?? '') 
-    manga['address'] = String(location.href.match(/http?s:\/\/\w*.\w+\/\w+\/\w+?.+[^(\/\d+.\d+)#]/)[0]) 
-    manga['address-page'] =  String('')
-	  manga['id'] = Number(location.pathname.match(/\d+/)[0])
-    manga['page'] = Number((location.hash.length) ? location.hash.replace('#', '') : 0)
-    manga['chapters'] = Number($(".cap")?.id?.match(/\d+/)[0] ?? 0) 
-    manga['current'] = Number((location.pathname.match(/(\d+\/(\d+))/)?.length)? location.pathname.match(/(\d+\/(\d+))/)[2] : 0)
-    manga['progress'] = 0
+    manga.title = String($$('.title')[0]?.textContent ?? $('.html-embed-3')?.children[0]?.children[0]?.textContent?.match(/(\w+.*[-])/)[0]?.slice(0, -2)) 
+	  manga.cover = String(document.querySelector('.image-3')?.src ?? null) 
+    manga.address = String(location.href.match(/http?s:\/\/\w*.\w+\/\w+\/\w+?.+[^(\/\d+.\d+)#]/)[0])
+	  manga.id = Number(location.pathname.match(/mh(\d+)/)[1])
+    manga.page = Number((location.hash.length) ? location.hash.replace('#', '') : 1)
+    manga.chapters = Number($(".cap")?.id?.match(/\d+/)[0] ?? 0) 
+    manga.current = Number((location.pathname.match(/(\d+\/(\d+))/)?.length)? location.pathname.match(/(\d+\/(\d+))/)[2] : 0)
+    manga.progress = 0
+    manga.status = String($('.btn-caps')?.textContent ?? null )
     
 	return manga
 
@@ -63,31 +63,54 @@ async function main(){
     currentManga = null;
     addStarToPage(manga)
 
-    chrome.storage.sync.set({'currentManga': manga }, function() {
-      console.log(`current manga found`)
+    window.addEventListener('hashchange', _ => {
+      manga.page = Number((location.hash.length) ? location.hash.replace('#', '') : 1)
+      
+      chrome.storage.sync.get(['favorites'], function({favorites}) {
+        
+        if(favorites[manga.id]) {
+          
+          let favorite = favorites[manga.id]
+          
+          if(favorite.current == manga.current)
+            favorite.page =  manga.page
+          else
+            return
+        
+          favorites[manga.id] = favorite
+          
+          chrome.storage.sync.set({'favorites': favorites }, function(){})
+        }
+      })
     })
-    
+
+    chrome.storage.sync.set({'currentManga': manga }, function(){})
+
     chrome.storage.sync.get(['favorites'], function({favorites}) {
-      debugger
-      if(favorites[`${ manga['id'] }`]){
+      
+      if(favorites[manga.id]){
 
-        let favorite = favorites[`${ manga['id'] }`]
+        let favorite = favorites[manga.id]
         
-        if( !favorite['chapters'] || favorite['chapters'] < manga['chapters'] )
-        {	
-          favorite['chapters'] =  manga['chapters']
+        if( !favorite.chapters || favorite.chapters < manga.chapters ){	
+          favorite.chapters =  manga.chapters
+        }
+        
+        if( !favorite.current || favorite.current < manga.current){
+          favorite.current = manga.current
         }
 
-        if( !favorite['cover']?.length && favorite['cover']?.length < manga['cover'] )
-        {
-          favorite['cover'] = manga['cover']
+        if(manga?.cover !== 'null'){
+          favorite.cover = manga.cover
         }
 
-        favorites[`${ manga['id'] }`] = favorite
+        if(manga.status !== 'null'){
+          favorite.status = manga.status
+        }
+
+        favorites[manga.id] = favorite
         
-        chrome.storage.sync.set({'favorites': favorites }, function() {
-          console.log(`Save data`)
-        })
+        chrome.storage.sync.set({'favorites': favorites }, function(){})
       }
     })
 
@@ -100,30 +123,26 @@ async function main(){
           	favorites = {}
         }
 
-        if(favorites[`${ manga['id'] }`]){
+        if(favorites[manga.id]){
 
-          let favorite = favorites[`${ manga['id'] }`]
+          let favorite = favorites[manga.id]
           
-          if( !favorite['chapters'] || favorite['chapters'] < manga['chapters'] )
-          {	
-            favorite['chapters'] =  manga['chapters']
+          if( !favorite.chapters || favorite.chapters < manga.chapters ){	
+            favorite.chapters =  manga.chapters
           }
 
-          if( !favorite['cover']?.length && favorite['cover']?.length < manga['cover'] )
-          {
-          	favorite['cover'] = manga['cover']
+          if( !favorite.cover?.length && favorite.cover?.length < manga.cover ){
+          	favorite.cover = manga.cover
           }
 
-          favorites[`${ manga['id'] }`] = favorite
+          favorites[manga.id] = favorite
 
         }
         else {
-          favorites[`${ manga['id'] }`] = manga
+          favorites[manga.id] = manga
         }
         
-        chrome.storage.sync.set({'favorites': favorites }, function() {
-          console.log(`Save data`)
-        })
+        chrome.storage.sync.set({'favorites': favorites }, function() {})
       })
     })
 
