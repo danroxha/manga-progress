72246be0affbd959d74bbf90ee489b03f269ae9b
Daniel Rocha <rochadaniel15@gmail.com>
2020-08-25 17:35:42 -0300

    refactor: Adicionado algoritmo de hash(SHA1)    -- Alterado a chave de acesso de dados do ID do manga para o HASH    -- Separado a atualizaçao de dados individual

--- src/daemon.js
+++ src/daemon.js
@@ -3,9 +3,7 @@ const $  = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
 
-function addStarToPage( manga )
-{
-
+function addStarToPage( manga ){
 
   let component = $$('.title')[0] ?? $('.heading-6')
 
@@ -19,6 +17,31 @@ function addStarToPage( manga )
   `
 }
 
+function updateManga(favorite, manga){
+ 
+  if( !favorite.id !== manga.id) {
+    favorite.id = manga.id
+  }
+  
+  if( !favorite.chapters || favorite.chapters < manga.chapters ){	
+    favorite.chapters =  manga.chapters
+  }
+  
+  if( !favorite.current || favorite.current < manga.current){
+    favorite.current = manga.current
+  }
+
+  if(manga?.cover !== 'null' || !favorite.cover?.length && favorite.cover?.length < manga.cover ){
+    favorite.cover = manga.cover
+  }
+
+  if(manga.status !== 'null'){
+    favorite.status = manga.status
+  }
+
+  
+  return favorite
+}
 
 function getMangaData(){
 
@@ -37,8 +60,8 @@ function getMangaData(){
     manga.current = Number((location.pathname.match(/(\d+\/(\d+))/)?.length)? location.pathname.match(/(\d+\/(\d+))/)[2] : 0)
     manga.progress = 0
     manga.status = String($('.btn-caps')?.textContent ?? null )
-
-
+    manga.hash = SHA1(manga.title)
+    
 	// CENTRALDEMANGAS.ONLINE/TITULOS/
 	
 	// manga.title = String($('h1')?.textContent ?? $$('.ui.breadcrumb > a')[2].textContent )
@@ -48,7 +71,7 @@ function getMangaData(){
 	// manga.cover = String( document.querySelector('.ui.relaxed.list div img').src ?? null )
 	
     
-	return manga
+	  return manga
 
   } catch(e) {
 	  throw new Error(e)
@@ -80,16 +103,16 @@ async function main(){
       
       chrome.storage.sync.get(['favorites'], function({favorites}) {
         
-        if(favorites[manga.id]) {
+        if(favorites[manga.hash]) {
           
-          let favorite = favorites[manga.id]
+          let favorite = favorites[manga.hash]
           
           if(favorite.current == manga.current)
             favorite.page =  manga.page
           else
             return
         
-          favorites[manga.id] = favorite
+          favorites[manga.hash] = favorite
           
           chrome.storage.sync.set({'favorites': favorites }, function(){})
         }
@@ -100,32 +123,9 @@ async function main(){
 
     chrome.storage.sync.get(['favorites'], function({favorites}) {
       
-      if(favorites[manga.id]){
-
-        let favorite = favorites[manga.id]
+      if(favorites[manga.hash]){
         
-        if( !favorite.chapters || favorite.chapters < manga.chapters ){	
-          favorite.chapters =  manga.chapters
-        }
-        
-        if( !favorite.current || favorite.current < manga.current){
-          favorite.current = manga.current
-        }
-
-        if(manga?.id === favorite?.id){
-          favorite.address = manga.address
-        }
-
-        if(manga?.cover !== 'null'){
-          favorite.cover = manga.cover
-        }
-
-        if(manga.status !== 'null'){
-          favorite.status = manga.status
-        }
-
-        favorites[manga.id] = favorite
-        
+        favorites[manga.hash] = updateManga(favorites[manga.hash], manga)
         chrome.storage.sync.set({'favorites': favorites }, function(){})
       }
     })
@@ -136,27 +136,14 @@ async function main(){
       chrome.storage.sync.get(['favorites'], function({favorites}) {
         
         if( isEmpty(favorites) ){
-          	favorites = {}
+          favorites = {}
         }
 
-        if(favorites[manga.id]){
 
-          let favorite = favorites[manga.id]
-          
-          if( !favorite.chapters || favorite.chapters < manga.chapters ){	
-            favorite.chapters =  manga.chapters
-          }
-
-          if( !favorite.cover?.length && favorite.cover?.length < manga.cover ){
-          	favorite.cover = manga.cover
-          }
-
-          favorites[manga.id] = favorite
-
-        }
-        else {
-          favorites[manga.id] = manga
-        }
+        if(favorites[manga.hash])
+          favorites[manga.hash] = updateManga(favorites[manga.hash], manga)
+        else 
+          favorites[manga.hash] = manga
         
         chrome.storage.sync.set({'favorites': favorites }, function() {})
       })
@@ -168,5 +155,173 @@ async function main(){
   
 }
 
+
+/**
+*
+*  Secure Hash Algorithm (SHA1)
+*  http://www.webtoolkit.info/
+*
+**/
+
+
+function SHA1 (msg) {
+  
+  function rotate_left(n,s) {
+    var t4 = ( n<<s ) | (n>>>(32-s));
+    return t4;
+  };
+
+  function lsb_hex(val) {
+    
+    var str="";
+    var i;
+    var vh;
+    var vl;
+
+    for( i=0; i<=6; i+=2 ) {
+      vh = (val>>>(i*4+4))&0x0f;
+      vl = (val>>>(i*4))&0x0f;
+      str += vh.toString(16) + vl.toString(16);
+    }
+
+    return str;
+  };
+
+  function cvt_hex(val) {
+    var str="";
+    var i;
+    var v;
+
+    for( i=7; i>=0; i-- ) {
+      v = (val>>>(i*4))&0x0f;
+      str += v.toString(16);
+    }
+    
+    return str;
+  };
+ 
+  function Utf8Encode(string) {
+    string = string.replace(/\r\n/g,"\n");
+    var utftext = "";
+    for (var n = 0; n < string.length; n++) {
+
+      var c = string.charCodeAt(n);
+      if (c < 128) {
+        utftext += String.fromCharCode(c);
+      }
+      else if((c > 127) && (c < 2048)) {
+        utftext += String.fromCharCode((c >> 6) | 192);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+      else {
+        utftext += String.fromCharCode((c >> 12) | 224);
+        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+    }
+    return utftext;
+  }
+
+  var blockstart;
+  var i, j;
+  var W = new Array(80);
+  var H0 = 0x67452301;
+  var H1 = 0xEFCDAB89;
+  var H2 = 0x98BADCFE;
+  var H3 = 0x10325476;
+  var H4 = 0xC3D2E1F0;
+  var A, B, C, D, E;
+  var temp;
+ 
+  msg = Utf8Encode(msg);
+
+  var msg_len = msg.length;
+  var word_array = new Array();
+
+  for( i=0; i<msg_len-3; i+=4 ) {
+    j = msg.charCodeAt(i)<<24 | msg.charCodeAt(i+1)<<16 |
+    msg.charCodeAt(i+2)<<8 | msg.charCodeAt(i+3);
+    word_array.push( j );
+  }
+
+  switch( msg_len % 4 ) {
+    case 0:
+      i = 0x080000000;
+      break;
+    case 1:
+      i = msg.charCodeAt(msg_len-1)<<24 | 0x0800000;
+      break;
+    case 2:
+      i = msg.charCodeAt(msg_len-2)<<24 | msg.charCodeAt(msg_len-1)<<16 | 0x08000;
+      break;
+    case 3:
+      i = msg.charCodeAt(msg_len-3)<<24 | msg.charCodeAt(msg_len-2)<<16 | msg.charCodeAt(msg_len-1)<<8    | 0x80;
+      break;
+    }
+
+  word_array.push( i );
+ 
+
+  while( (word_array.length % 16) != 14 ) word_array.push( 0 );
+    
+    word_array.push( msg_len>>>29 );
+    word_array.push( (msg_len<<3)&0x0ffffffff );
+
+    for ( blockstart=0; blockstart<word_array.length; blockstart+=16 ) {
+      for( i=0; i<16; i++ ) W[i] = word_array[blockstart+i];
+      for( i=16; i<=79; i++ ) W[i] = rotate_left(W[i-3] ^ W[i-8] ^ W[i-14] ^ W[i-16], 1);
+      A = H0;
+      B = H1;
+      C = H2;
+      D = H3;
+      E = H4;
+
+      for( i= 0; i<=19; i++ ) {
+        temp = (rotate_left(A,5) + ((B&C) | (~B&D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
+        E = D;
+        D = C;
+        C = rotate_left(B,30);
+        B = A;
+        A = temp;
+      }
+
+      for( i=20; i<=39; i++ ) {
+        temp = (rotate_left(A,5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
+        E = D;
+        D = C;
+        C = rotate_left(B,30);
+        B = A;
+        A = temp;
+      }
+
+      for( i=40; i<=59; i++ ) {
+        temp = (rotate_left(A,5) + ((B&C) | (B&D) | (C&D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
+        E = D;
+        D = C;
+        C = rotate_left(B,30);
+        B = A;
+        A = temp;
+      }
+
+      for( i=60; i<=79; i++ ) {
+        temp = (rotate_left(A,5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
+        E = D;
+        D = C;
+        C = rotate_left(B,30);
+        B = A;
+        A = temp;
+      }
+
+      H0 = (H0 + A) & 0x0ffffffff;
+      H1 = (H1 + B) & 0x0ffffffff;
+      H2 = (H2 + C) & 0x0ffffffff;
+      H3 = (H3 + D) & 0x0ffffffff;
+      H4 = (H4 + E) & 0x0ffffffff;
+    }
+
+  var temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
+
+  return temp.toLowerCase();
+}
 
 main()
