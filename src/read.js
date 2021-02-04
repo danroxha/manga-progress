b627ee47f725fb21113c31bc670adf18f904056a
Daniel Rocha <rochadaniel15@gmail.com>
2021-02-04 13:59:00 -0300

    refactor: Melhoria na legibilidade    - criado metodo para carregar dados da pagina    - criado metodo para limpar a pagina    - criado metodo para construir uma barra de navegaçao entre as paginas

--- src/read.js
+++ src/read.js
@@ -1,105 +1,103 @@
-function changeReadMode(title) {
-
-  const pages = Array.from(document.getElementsByTagName('img'))
-
-  const nodeSection = document.createElement('section')
-  const nodeTitle   = document.createElement('title')
-
-  nodeSection.setAttribute('class', 'focus-read')
-  nodeTitle.innerText = title
-
-  pages.forEach(page => {
-    nodeSection.appendChild(page)
-  })
-
-  document.getElementsByTagName("html")[0].innerHTML = "";
-  document.head.appendChild(nodeTitle)
-  document.body.setAttribute('class', 'bground')
-  document.body.appendChild(nodeSection)
-
-}
-
-function createBarFooter(pageData) {
-
-
-  const { url, title, pageNumber, chapters } = pageData
-  const request = { url, page: pageNumber, chapters }
-
-  const nodeFooter  = document.createElement('footer')
-  const nodeTitle   = document.createElement('h1')
-  const nodeAnchor  = document.createElement('a')
-  const nodeSection = document.createElement('section')
-  const nodeButton  = new Array(2)
+const readMode = {
+  data: {url:'', title:'', page: 0, chapters: [] },
   
-  nodeButton[0] = document.createElement('button')
-  nodeButton[1] = document.createElement('button')
-
-  nodeButton[0].addEventListener('click', () => previousPage(request))
-  nodeButton[1].addEventListener('click', () => nextPage(request))
-  nodeButton[0].innerText = '<<'
-  nodeButton[1].innerText = '>>'
-
-  nodeFooter.setAttribute('class', 'bar-footer')
-  nodeAnchor.innerText = title
-  nodeAnchor.href = url
-  nodeTitle.appendChild(nodeAnchor)
-
-  nodeFooter.appendChild(nodeTitle)
-
-  nodeButton.forEach(button => {
-    nodeSection.appendChild(button)
-  })
-
-  nodeFooter.appendChild(nodeSection)
+  enable() {
+    this.loadData()
+    this.clearPage()
+    this.createBar()
+  },
   
-  document.body.appendChild(nodeFooter)
-  document.body.addEventListener('keyup', e => {
-    if(e.code == 'ArrowLeft') previousPage(request)
-    if(e.code == 'ArrowRight') nextPage(request)
-  })
+  canEnable() {
+    return !!location.href.match(/mangahost\w+.com\/manga\/\w+.+\/\d+/)
+  },
 
-  function nextPage(request) {
-          
-    const pageIndex = request.chapters.indexOf(request.page)
-    const nextIndex = request.chapters[pageIndex + 1]
+  loadData() {
+    this.data.url      = String(location.href.match(/http?s:\/\/\w*.\w+\/\w+\/\w+?.+[^(\/\d+.\d+)#]/)[0])
+    this.data.title    = String($$('.title')[0]?.textContent ?? $('.html-embed-3')?.children[0]?.children[0]?.textContent?.match(/(\w+.*[-])/)[0]?.slice(0, -2))
+    this.data.page     = Number((location.pathname.match(/\w+\d+\/(\d+|\w+.+)$/)?.length)? location.pathname.match(/\w+\d+\/(\d+|\w+.+)$/)[1] : 0)
+    this.data.chapters = Array.from(document.querySelector('select').getElementsByTagName('option')).map(({value}) => Number(value), [])
+    
+    this.data.chapters.sort((a, b) => {
+      if(a > b) return 1
+      if(a < b) return -1
+      return 0
+    })
+  },
 
-    if( nextIndex )
-      window.location.assign(`${request.url}/${nextIndex}`)
+  clearPage() {
+    const pages = Array.from(document.getElementsByTagName('img'))
+
+    const nodeSection = document.createElement('section')
+    const nodeTitle   = document.createElement('title')
+
+    nodeSection.setAttribute('class', 'focus-read')
+    nodeTitle.innerText = this.data.title
+
+    pages.forEach(page => {
+      nodeSection.appendChild(page)
+    })
+
+    document.getElementsByTagName("html")[0].innerHTML = "";
+    document.head.appendChild(nodeTitle)
+    document.body.setAttribute('class', 'bground')
+    document.body.appendChild(nodeSection)
+  },
+
+  createBar() {
+    const nodeFooter  = document.createElement('footer')
+    const nodeTitle   = document.createElement('h1')
+    const nodeAnchor  = document.createElement('a')
+    const nodeSection = document.createElement('section')
+    const nodeButton  = new Array(2)
+
+    nodeButton[0] = document.createElement('button')
+    nodeButton[1] = document.createElement('button')
+
+    nodeButton[0].addEventListener('click', this.previousPage)
+    nodeButton[1].addEventListener('click', this.nextPage)
+    nodeButton[0].innerText = '<<'
+    nodeButton[1].innerText = '>>'
+
+    nodeFooter.setAttribute('class', 'bar-footer')
+    nodeAnchor.innerText = this.data.title
+    nodeAnchor.href = this.data.url
+    nodeTitle.appendChild(nodeAnchor)
+
+    nodeFooter.appendChild(nodeTitle)
+
+    nodeButton.forEach(button => {
+      nodeSection.appendChild(button)
+    })
+
+    nodeFooter.appendChild(nodeSection)
+    
+    document.body.appendChild(nodeFooter)
+    document.body.addEventListener('keyup', e => {
+      if(e.code == 'ArrowLeft') this.previousPage()
+      if(e.code == 'ArrowRight') this.nextPage()
+    })
+  },
+
+  nextPage() {
+    const pageIndex = this.data.chapters.indexOf(this.data.page)
+    const nextPage = this.data.chapters[pageIndex + 1]
+
+    if( nextPage )
+      location.assign(`${this.data.url}/${nextPage}`)
     else 
-      alert(`Last Chapter: ${request.page}`)
-  
-  }
+      alert(`Last Chapter: ${this.data.page}`)
+  },
 
-  function previousPage(request) {
+  previousPage() {
+    const pageIndex = this.data.chapters.indexOf(this.data.page)
+    const previousPage = this.data.chapters[pageIndex - 1]
 
-    const pageIndex = request.chapters.indexOf(request.page)
-    const nextIndex = request.chapters[pageIndex - 1]
-
-    if( nextIndex )
-      window.location.assign(`${request.url}/${nextIndex}`)
+    if( previousPage )
+      location.assign(`${this.data.url}/${previousPage}`)
     else
-      alert(`First Chapter: ${request.page}`)
-     
-  }
+      alert(`First Chapter: ${this.data.page}`)
+  },
 }
 
-function main() {
-  
-  const url = String(location.href.match(/http?s:\/\/\w*.\w+\/\w+\/\w+?.+[^(\/\d+.\d+)#]/)[0])
-  const title = String($$('.title')[0]?.textContent ?? $('.html-embed-3')?.children[0]?.children[0]?.textContent?.match(/(\w+.*[-])/)[0]?.slice(0, -2))
-  const pageNumber = Number((location.pathname.match(/\w+\d+\/(\d+|\w+.+)$/)?.length)? location.pathname.match(/\w+\d+\/(\d+|\w+.+)$/)[1] : 0)
-  const chapters = Array.from(document.querySelector('select').getElementsByTagName('option')).map(({value}) => Number(value), [])
-  
-  chapters.sort((a, b) => {
-    if(a > b) return 1
-    if(a < b) return -1
-    return 0
-  })
-
-  changeReadMode(title)
-  createBarFooter({url, title, pageNumber, chapters })
-}
-
-const isPageRead = !!location.href.match(/mangahost\w+.com\/manga\/\w+.+\/\d+/)
-if(isPageRead)
-  main()
+if(readMode.canEnable())
+  readMode.enable()
