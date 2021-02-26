2c2ec37dbfed450f060d8559bcf7d66cfdaf2313
Daniel Rocha <rochadaniel15@gmail.com>
2021-02-26 10:36:15 -0300

    fix: Corrigo o posicionamento do cartão flutuante com informações do mangá, etc. issue: Close #17

--- src/popup/component/CardInfo/index.js
+++ src/popup/component/CardInfo/index.js
@@ -4,7 +4,7 @@ export default {
     <dl
       v-show='!!cardInformation.enable'
       id="card-float" 
-      :style='{top:cardInformation.y+"px", left:cardInformation.x+"px", height: (displaySetting.modes.select == "list")? "60px": "70px"}'
+      :style='{top: cardInformation.y+"px", left:cardInformation.x+"px", height: (displaySetting.modes.select == "list")? "60px": "70px", width: cardInformation.width +"px"}'
     >
         <dt v-if='displaySetting.modes.select == "grid"'>Title</dt>
         <dd v-if='displaySetting.modes.select == "grid"'>{{cardInformation.data?.title}}</dd>
--- src/popup/store/cardState/index.js
+++ src/popup/store/cardState/index.js
@@ -9,6 +9,7 @@ export default {
 
     cardInformation: {
       x: 0, y: 0, data: null,
+      width: 0, height: 0,
       enable: false,
     },
   },
@@ -19,13 +20,19 @@ export default {
       const [event, data] = value
       const margin = 10
 
-      // if(this.mode === 'read' )
-      //   return
-
+      state.cardInformation.height = 70
+      state.cardInformation.width = 170
       state.cardInformation.enable = true
       state.cardInformation.data = data
       state.cardInformation.x = event.x + margin
       state.cardInformation.y = event.y + margin
+
+      const { x, y, width, height } = state.cardInformation
+      if( (width + x + margin) > event.view.innerWidth)
+        state.cardInformation.x -= (width + x) - event.view.innerWidth + margin
+
+      if( (height + y) > event.view.innerHeight)
+        state.cardInformation.y -= (height + y) - event.view.innerHeight
     },
 
     disableCardInformation(state) {
@@ -34,13 +41,11 @@ export default {
 
     dragstart(state, event) {
       state.cardInformation.enable = false
-      // state.disableFieldFilter()
       state.removing.status = !state.removing.status
       state.removing.component = event.target
     },
 
     dragend(state) {
-      // state.enableFieldFilter()
       state.removing.status = !state.removing.status
     },
 
